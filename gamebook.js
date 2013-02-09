3c6810ba51b62cd096fc14a01c89891532094541
Christian Jauvin <cjauvin@gmail.com>
2013-02-09 13:28:59 -0500

    newline

--- gamebook.js
+++ gamebook.js
@@ -118,7 +118,8 @@ $(document).ready(function($) {
     ],
 
     combat_results_ranges = [
-        [Number.NEGATIVE_INFINITY, -11], [-10,-9], [-8,-7], [-6,-5], [-4,-3], [-2,-1], [0,0], [1,2], [3,4], [5,6], [7,8], [9,10], [11, Number.POSITIVE_INFINITY]
+        [Number.NEGATIVE_INFINITY, -11], [-10,-9], [-8,-7], [-6,-5], [-4,-3], [-2,-1], [0,0], [1,2], [3,4], [5,6],
+        [7,8], [9,10], [11, Number.POSITIVE_INFINITY]
     ],
 
     colors = {
