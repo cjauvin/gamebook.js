7d2db4f129a726526bdf3f9cf0ac1c495ef6c4a5
Christian Jauvin <cjauvin@gmail.com>
2013-02-11 21:12:10 -0500

    namedtuple -> dict

--- xml2json.py
+++ xml2json.py
@@ -1,11 +1,6 @@
-import re, textwrap
+import re, textwrap, json
 from lxml import etree
-from collections import namedtuple
 
-
-Section = namedtuple('Section', ['sect_id', 'wrapped_text', 'text', 'options', 'is_random_pick'])
-Option = namedtuple('Option', ['sect_id', 'wrapped_text', 'text', 'range'])
-Enemy = namedtuple('Enemy', ['name', 'combat_skill', 'endurance', 'win_option'])
 
 def processPara(para):
     para = re.sub('</?p/?>', '', para)
@@ -23,40 +18,61 @@ parser = etree.XMLParser(resolve_entities=False)
 tree = etree.parse('fotw.xml', parser=parser)
 root = tree.getroot()
 
-sections = []
+sections = {}
+
+sect_id = '240'
 
 #for section in root.findall('.//section[@class="numbered"]')[1:]:
-for sect_elem in root.findall('.//section[@id="sect348"]'):
+for sect_elem in root.findall('.//section[@id="sect%s"]' % sect_id):
     sect_paras = []
     options = []
+    combat = {}
     enemies = []
     rnt_found = False
+    is_special = False
     for item in sect_elem.find('data'):
         s = etree.tostring(item).strip()
         if item.tag == 'p':
             if '<a idref="random">Random Number Table</a>' in s:
                 rnt_found = True
                 s = s.replace('<a idref="random">Random Number Table</a>', 'Random Number Table')
+            for a in ['COMBAT SKILL', 'ENDURANCE']:
+                if '<typ class="attribute">%s</typ>' % a in s:
+                    s = s.replace('<typ class="attribute">%s</typ>' % a, a)
+                    is_special = True
             sect_paras.append(processPara(s))
-        if item.tag == 'combat':
-            e = Enemy(item.find('.//enemy').text,
-                                 int(item.find('..//enemy-attribute[@class="combatskill"]').text),
-                                 int(item.find('..//enemy-attribute[@class="endurance"]').text), None)
+        elif item.tag == 'combat':
+            e = {'name': item.find('.//enemy').text,
+                 'combat_skill': int(item.find('..//enemy-attribute[@class="combatskill"]').text),
+                 'endurance': int(item.find('..//enemy-attribute[@class="endurance"]').text)}
             enemies.append(e)
-            sect_paras.append(['%s: COMBAT SKILL %d, ENDURANCE %d' % (e.name, e.combat_skill, e.endurance)])
-        if item.tag == 'choice':
+            sect_paras.append(['%s: COMBAT SKILL %d, ENDURANCE %d' % (e['name'], e['combat_skill'], e['endurance'])])
+        elif item.tag == 'choice':
             sect_id = re.search('idref="sect(\d+)"', s).group(1)
-            p = processPara(s)
-            options.append(Option(sect_id, processPara(s), '\n'.join(p), None))
+            options.append({'section': sect_id, 'text': '\n'.join(processPara(s))})
     sect_text = '\n\n'.join(['\n'.join(p) for p in sect_paras])
-    #print sect_text
     is_random_pick = False
-    if rnt_found and re.search('\d-\d', options[0].text):
+    if rnt_found and re.search('\d-\d', options[0]['text']):
         is_random_pick = True
-        for i, opt in enumerate(options):
-            options[i] = opt._replace(range=re.search('(\d)-(\d)', opt.text).groups())
-    section = Section(sect_elem.find('.//title').text, sect_paras, sect_text, options, is_random_pick)
-    sections.append(section)
+        for opt in options:
+            opt['range'] = [int(s) for s in re.search('(\d)-(\d)', opt['text']).groups()]
+    if enemies:
+        combat['enemies'] = enemies
+        for opt in reversed(options):
+            if 'win' in opt['text']:
+                combat['win'] = opt
+                options.remove(opt)
+            elif 'evade' in opt['text']:
+                combat['evasion'] = opt
+                options.remove(opt)
+                if re.search('at any time|stage', opt['text']):
+                    combat['evasion']['n_rounds'] = 0
+                elif 'after two rounds' in opt['text']:
+                    combat['evasion']['n_rounds'] = 2
+    section = {'text': sect_text, 'options': options}
+    if combat: section['combat'] = combat
+    if is_random_pick: section['is_random_pick'] = True
+    if is_special: section['is_special'] = True
+    print json.dumps(section, indent=4)
+    sections[sect_elem.find('.//title').text] = section
     exit()
-
-
